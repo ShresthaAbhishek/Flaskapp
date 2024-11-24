ec3ff858819c592892e934cfcfc8c85db6b8c16b
ShresthaAbhishek <shresthaabhishek18@gmail.com>
2024-11-23 23:56:37 -0600

    New Commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy;
 from datetime import datetime
 
@@ -8,11 +8,11 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
-#Data class this is out model
+#Data class; this is our model (rows)
 class MyTask(db.Model):
-    id = db.Column(db.integer, primary_key=True)
-    content = db.Column(db.string(100), nullable=False)
-    completed = db.Column(db.integer, default=0)
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Integer, default=0)
     created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
@@ -20,10 +20,51 @@ class MyTask(db.Model):
 
 
 
-#Setting up route
-@app.route("/")
+#Setting up route HOME PAGE
+@app.route("/", methods = ["POST", "GET"])
 def index():
-    return render_template("index.html")
+    #ADD TASK
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = MyTask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"Error:{e}")
+            return f"ERROR:{e}"
+        #See all current Tasks
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+        return render_template("index.html", tasks=tasks)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ in "__main__":
+    with app.app_context():
+        db.create_all()
+    #use context manager
     app.run(debug=True)
